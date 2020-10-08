196c82bed99d66adbc33160369f51933d90fe933
Ian Pons <ianpons2107@gmail.com>
2020-10-08 20:30:38 -0300

    Initial Server

---
+++ bot.js
@@ -0,0 +1,76 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve('.env') })
+const discord = require('discord.js');
+const config = require('./config.json');
+
+const bot = new discord.Client();
+
+
+bot.once('ready', () => {
+    console.log('Online');
+})
+
+if (command === 'serverinfo') {
+    message.reply(`Número de membros: ${message.guild.memberCount}\n Região do servidor: ${message.guild.region}`);
+}
+
+if (command === 'cargos') {
+    if (args[0] === 'add') {
+        let erros = [];
+        for (let i = 1; i < args.length; i++) {
+            const role = message.guild.roles.cache.find(role => role.name === args[i]);
+            const member = message.member;
+
+            if (!role || config.cargosPrivilegiados.includes(args[i])) {
+                erros.push(args[i].toString());
+            }
+            else {
+                member.roles.add(role).catch(console.error);
+            }
+        }
+        if (erros.length) {
+            message.reply(`Os cargos a seguir não existem ou você não possui permissão para usa-los: \n ${erros}`);
+        }
+        else { message.react('✔️').catch(console.error); }
+    }
+
+    else if (args[0] === 'delete') {
+        let erros = [];
+        for (let i = 1; i < args.length; i++) {
+            const role = message.guild.roles.cache.find(role => role.name === args[i]);
+            const member = message.member;
+
+            if (role) {
+                member.roles.remove(role).catch(console.error);
+            }
+            else {
+                erros.push(args[i].toString());
+            }
+        }
+        if (erros.length) {
+            message.reply(`Os cargos a seguir não existem ou você não possui permissão para deleta-los: \n ${erros}`);
+        }
+        else { message.react('✔️').catch(console.error); }
+    }
+
+    else if (args[0] === 'count') {
+        let count = 0;
+        const role = message.guild.roles.cache.find(role => role.name === args[1]);
+
+        if (!role) {
+            message.reply(`O cargo "${args[1]}" não foi encontrado.`);
+            return
+        }
+        const members = message.guild.members.cache;
+
+        members.forEach(m => {
+            if (m.roles.cache.find(r => r === role))
+                count++;
+        })
+
+        message.reply(`Há ${count} membros com o cargo ${args[1]} `);
+
+    }
+}
+
+bot.login(process.env.DISCORD_TOKEN);
